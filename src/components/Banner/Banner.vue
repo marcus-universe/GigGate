f85532438f2bf68a49c9dc21319ad0b56ed1f1e4
Marcus Tutorials <marcusharting@web.de>
2022-06-15 17:33:43 +0200

    BannerComponent Erstellt

---
+++ src/components/Banner/Banner.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="SecondBanners" :class="bannerClass">
+
+    </div>
+</template>
+<script>
+export default {
+    props: [
+        "boxWidth" = 40,
+        "bannerClass" = ["CommunityBanner", "EventsBanner"]
+    ],
+}
+</script>
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,19 @@
 <template>
 <section class="home">
-
 <div class="JetztJammenSection">
     <div class="JetztJammenBanner"></div>
-
 </div>
-
 <div class="HomeSection2">
-<div class="SecondBanners EventsBanner"></div>
-<div class="SecondBanners CommunityBanner"></div>
-
+<div class="SecondBanners EventsBanner">
 </div>
-
+<div class="SecondBanners CommunityBanner">
+</div>
+</div>
 </section>
 </template>
 
 <script>
+export default {
 
+}
 </script>
