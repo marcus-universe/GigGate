9e0604a2d7c42b09ed3fe5e89e72ef46f62a13e0
Marcus Tutorials <marcusharting@web.de>
2022-06-23 11:55:15 +0200

    installed _VueX_notFinished

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,27 +17,21 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'no-debugger': 'off',
-    "indent": ["error", 2],
     "no-multiple-template-roots": "off",
-    "no-multiple-empty-lines": ["error", { "max": 6, "maxBOF": 0, "maxEOF": 0 }],
+    "no-multiple-empty-lines": ["error", { max: 6, maxBOF: 0, maxEOF: 0 }],
     "eol-last": ["off", "always"],
     "space-before-function-paren": ["off", "always"],
     "comma-dangle": ["off", "always"],
-    "indent": ["off", 6],
-    "no-trailing-spaces": ["off", { "skipBlankLines": true }],
+    indent: ["off", 6],
+    "no-trailing-spaces": ["off", { skipBlankLines: true }],
     "object-curly-spacing": ["off", "always"],
-    "quotes": ["off", "double"],
-    "dot-notation": ["off", { "allowKeywords": true }],
-    "multi-word-component-names": [0, { "componentName": "^[a-zA-Z]+$" }],
-    "vue/multi-word-component-names": ["off", { "componentName": "^[a-zA-Z]+$" }],
+    quotes: ["off", "double"],
+    "dot-notation": ["off", { allowKeywords: true }],
+    "multi-word-component-names": [0, { componentName: "^[a-zA-Z]+$" }],
+    "vue/multi-word-component-names": ["off", { componentName: "^[a-zA-Z]+$" }],
+    "no-dupe-keys": ["off"]
     // "linebreak-style": ["error", "windows"],
     // "quotes": ["error", "single"],
 
   },
-
-  'extends': [
-    // 'plugin:vue/essential',
-    'plugin:vue/vue3-essential',
-    '@vue/standard'
-  ]
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue'
 import router from './router'
 import Vue3Lottie from 'vue3-lottie'
 import 'vue3-lottie/dist/style.css'
+import store from './store'
 
 
-const app = createApp(App)
+const app = createApp(App).use(store)
 
 app.use(router).use(Vue3Lottie).mount('#app')
